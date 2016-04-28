8faa032445f9d0f1185ea7ba042cd2226aca0464
Masha <MashaLypa@yandex.ua>
2016-04-28 12:51:18 +0300

    Добавляем еще две точки класса Point и выводим на экран

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -15,6 +15,12 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');            
             p2.Draw();
 
+            Point p3 = new Point(5, 6, '-');
+            p3.Draw();
+
+            Point p4 = new Point(6, 7, '+');
+            p4.Draw();
+
             List<int> numList = new List<int>();
             numList.Add(0);
             numList.Add(1);
