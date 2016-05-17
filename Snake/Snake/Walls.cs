e15c6dc20cac6eebb915460509912124cfbf557e
Masha <MashaLypa@yandex.ua>
2016-05-17 14:28:07 +0300

    Add walls.

--- Snake/Snake/Walls.cs
+++ Snake/Snake/Walls.cs
@@ -14,10 +14,10 @@ namespace Snake
             wallList = new List<Figure>();
 
             //Отрисовка рамочки
-            HorizontalLine upLine = new HorizontalLine(0, mapWidth - 2, 0, '+');
-            HorizontalLine downLine = new HorizontalLine(0, mapWidth - 2, mapHeight - 1, '+');
-            VerticalLine leftLine = new VerticalLine(0, mapHeight - 1, 0, '+');
-            VerticalLine rightLine = new VerticalLine(0, mapHeight - 1, mapWidth - 2, '+');
+            HorizontalLine upLine = new HorizontalLine(1, mapWidth - 2, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(1, mapWidth - 2, mapHeight - 1, '+');
+            VerticalLine leftLine = new VerticalLine(0, 1, mapHeight - 2, '+');
+            VerticalLine rightLine = new VerticalLine(mapWidth - 1, 1, mapHeight - 2, '+');
 
             wallList.Add(upLine);
             wallList.Add(downLine);
